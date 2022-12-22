658057e4fbf263474ef6322f30d1e9a61c66c3f3
Exstosy <47812846+Exstosy@users.noreply.github.com>
2022-12-22 03:40:28 +0200

    Add files via upload

---
+++ Program29.cs
@@ -0,0 +1,10 @@
+﻿ Console.Clear();
+ Console.WriteLine("Введите 8 чисел массива : ");
+            int[] a = new int[8];
+            for(int i = 0; i<8;i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+                
+            }
+            Console.WriteLine("\n: {0}",String.Join(" ",a));
+            
