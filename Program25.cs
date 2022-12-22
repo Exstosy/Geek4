9ea32ab47834895962b9fd2fb3d99787794f1465
Exstosy <47812846+Exstosy@users.noreply.github.com>
2022-12-22 03:01:01 +0200

    Add files via upload

---
+++ Program25.cs
@@ -0,0 +1,19 @@
+﻿int number1 = ReadInt("Введите число A: ");
+int number2 = ReadInt("Введите число B: ");
+ToDegree(number1, number2);
+
+void ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
